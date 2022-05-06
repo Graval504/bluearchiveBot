cf8c2738de2561ce21c7907ec4e433a89ec52573
Graval504 <jekromu32@gmail.com>
2022-05-07 01:42:09 +0900

    make getting html of character list

--- characterlist.go
+++ characterlist.go
@@ -1,15 +1,25 @@
 package bluearchiveBot
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
+	"log"
+	"os/exec"
 )
 
-func GetCharacterList() {
-	url := "https://namu.wiki/w/%ED%8B%80:%EB%B8%94%EB%A3%A8%20%EC%95%84%EC%B9%B4%EC%9D%B4%EB%B8%8C/%ED%95%99%EC%83%9D%EB%AA%85%EB%B6%80"
-	res, _ := http.Get(url)
-	doc, _ := goquery.NewDocumentFromReader(res.Body)
-	fmt.Println(doc.Find("h3").Text())
+func GetCharacterList() string {
+	htmlpy := exec.Command("python", "-c",
+		"import requests; "+
+			"from bs4 import BeautifulSoup as bs; "+
+			"request_headers={'User-Agent':('PythonScraper'),}; "+
+			"response = requests.get('https://namu.wiki/w/%ED%8B%80:%EB%B8%94%EB%A3%A8%20%EC%95%84%EC%B9%B4%EC%9D%B4%EB%B8%8C/%ED%95%99%EC%83%9D%EB%AA%85%EB%B6%80',headers=request_headers); "+
+			"html = bs(response.text,'html.parser'); "+
+			"print(str(html), end = '')")
+	out, err := htmlpy.CombinedOutput()
+	checkErr(err)
+	return string(out)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/Graval504/bluearchiveBot
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0 
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/bwmarrin/discordgo v0.25.0
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/bwmarrin/discordgo v0.25.0 
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
